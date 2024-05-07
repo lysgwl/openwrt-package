798031d5b9f232d24ccb129e94f9228b7aa30d4c
unknown <lysgwl@163.com>
2024-05-07 15:16:21 +0800

    2024.05.07 15:16

--- .github/scripts/run-scripts.sh
+++ .github/scripts/run-scripts.sh
@@ -353,7 +353,7 @@ function check_git_commit()
 	if [ -n "$has_changes" ]; then
 		current_date=$(date '+%Y-%m-%d')
 		
-		git commit -a -m "commit repository  changes on $current_date"
+		git commit -a -m "commit repository changes on ${current_date}"
 		git push git@github.com:lysgwl/openwrt-package.git HEAD:master
 	fi
 	
