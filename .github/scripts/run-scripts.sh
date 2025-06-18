3a19ebf9d44f28a06d5b053b72bbdd4289414c7b
unknown <lysgwl@163.com>
2025-06-18 19:20:08 +0800

    update check_git_commit

--- .github/scripts/run-scripts.sh
+++ .github/scripts/run-scripts.sh
@@ -4,6 +4,9 @@
 # 提交代码
 function check_git_commit() 
 {
+	# 远程仓库URL
+	#local remote_repo=$1
+	
 	# 目标目录路径
 	local target_path=$1
 	
@@ -41,6 +44,9 @@ function check_git_commit()
 		popd >/dev/null
 		return 4
 	fi
+	
+	local remote_url=$(git remote get-url "$remote_name")
+	echo "[INFO] 远程仓库URL: $remote_url"
 	
 	# 解决换行符警告配置
 	git config core.autocrlf false			# 禁用自动换行符转换
@@ -86,9 +92,9 @@ function check_git_commit()
 		local commit_hash=$(git rev-parse --short HEAD)
 		
 		# 推送变更
-		echo "[INFO] 推送变更到: $remote_name/$current_branch..."
-		if ! git push "$remote_name" "HEAD:$current_branch"; then
-			echo "[ERROR] 推送失败, 请检查! $remote_name/$current_branch"
+		echo "[INFO] 推送变更到: $remote_url/$current_branch..."
+		if ! git push "$remote_url" "HEAD:$current_branch"; then
+			echo "[ERROR] 推送失败, 请检查! $remote_url/$current_branch"
 			popd >/dev/null
 			return 7
 		fi
