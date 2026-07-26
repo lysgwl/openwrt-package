9d93eae3a0dc502d8210b8a809ef6341abd9f083
unknown <lysgwl@163.com>
2026-07-26 11:56:14 +0800

    20260726 11:56 测试提交

--- .github/scripts/update_scripts.sh
+++ .github/scripts/update_scripts.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+echo "SYSTEM_DIR=$SYSTEM_DIR"
+echo "SCRIPT_ROOT=$SCRIPT_ROOT"
+echo "SYSTEM_ROOT=$SYSTEM_ROOT"
+echo "UPDATE_SCRIPT=$UPDATE_SCRIPT"
+
 update_other_package()
 {
+	echo "update_other_package"
 }
